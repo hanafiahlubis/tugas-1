40ab9516e03b6d65c6e17a4fffb809e6218fd9f2
Ali Hanafiah <hanafiaha308@gmail.com>
2023-05-23 10:30:56 +0700

    selseai

---
+++ soal-1.js
@@ -0,0 +1,14 @@
+let {nama,nim,email} = {
+    nama : "Ali Hanafiah",
+    nim : "02042111004",
+    email : "hanafiaha308@gmail.com"
+}
+
+console.log(nama);
+console.log(nim);
+console.log(email);
+
+const arr =["faisal","dede"];
+const arr1 =["sawal","ahmed"];
+const arr2 =["farhan","azhari"];
+console.log([...arr,...arr1,...arr2]);
